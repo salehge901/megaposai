9bfc9cf24d5e6ac5403cb5cffd96f90c21ef1964
M Saleh <salehg901@gmail.com>
2025-06-03 05:42:26 +0500

    fix

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -27,33 +27,33 @@ class PHP_Email_Form
         $mail = new PHPMailer(true);
 
         try {
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'mail.megapos.ltd.uk';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'hello@megapos.ltd.uk';                     //SMTP username
-            $mail->Password   = '~n[pg%SO9MYm';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            // $mail->isSMTP();                                            //Send using SMTP
+            // $mail->Host       = 'mail.megapos.ltd.uk';                     //Set the SMTP server to send through
+            // $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            // $mail->Username   = 'hello@megapos.ltd.uk';                     //SMTP username
+            // $mail->Password   = '~n[pg%SO9MYm';                               //SMTP password
+            // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            // $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-            //Recipients
-            $mail->setFrom('hello@megapos.ltd.uk', 'Megapos Ltd');
-            $mail->addAddress('salehge901@gmail.com', 'Saleh');     //Add a recipient                        
-            // $mail->addReplyTo('info@example.com', 'Information');
-            // $mail->addCC('cc@example.com');
-            // $mail->addBCC('bcc@example.com');
+            // //Recipients
+            // $mail->setFrom('hello@megapos.ltd.uk', 'Megapos Ltd');
+            // $mail->addAddress('salehge901@gmail.com', 'Saleh');     //Add a recipient                        
+            // // $mail->addReplyTo('info@example.com', 'Information');
+            // // $mail->addCC('cc@example.com');
+            // // $mail->addBCC('bcc@example.com');
 
-            //Attachments
-            // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+            // //Attachments
+            // // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            // // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            // //Content
+            // $mail->isHTML(true);                                  //Set email format to HTML
+            // $mail->Subject = 'Here is the subject';
+            // $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            // // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-            $mail->send();
+            // $mail->send();
             // SMTP settings
             // if ($this->smtp && is_array($this->smtp)) {
             //     $mail->isSMTP();
